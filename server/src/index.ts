0ce55b4e74a6e7f6b99c69378c7d7e06366b1189
Alexandre billereau <alexandre.mr.billereau@gmail.com>
2023-02-15 11:56:53 -0500

    server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,22 @@
-import express from "express";
+import express    from "express"
+import helmet     from "helmet"
+import bodyParser from "body-parser"
+import cors       from "cors"
+import morgan     from "morgan"
 
 const app  = express()
 const port = 8080
 
+// temporary solution
+let dataBase: {} = []
+
+app.use(helmet())
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('combined'))
+
 app.get("/", (req, res)=>{
-  res.send( "<h1>Yata</h1>" )
+  res.send( dataBase )
 })
 
 app.listen(port, ()=>{
